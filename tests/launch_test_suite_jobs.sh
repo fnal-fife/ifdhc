31fd18723b1cfce17b92766c677e4167340db1f7
Marc Mengel <mengel@fnal.gov>
2014-07-18 13:11:58 -0500

    test sutie job progress

--- tests/launch_test_suite_jobs.sh
+++ tests/launch_test_suite_jobs.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+export TEST_USER=$USER
+
 # for site in FNAL_nova Wisconsin SMU Nebraska UCSD 
 for site in FNAL_nova 
 do
     jobsub -g \
         --OS SL6 \
+        -e TEST_USER \
 	-l "+JobType = \"MC\"" \
  	--site $site \
 	--tar_file_name=/nova/app/users/$USER/ifdhc_$site.tar \

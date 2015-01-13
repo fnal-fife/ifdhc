dac720522ed9425fbbc806dce7af56464e349181
Marc Mengel <mengel@fnal.gov>
2015-01-13 11:29:28 -0600

    make ls, locate exit nonzero if nothing found

--- ifdh/h_to_main.sh
+++ ifdh/h_to_main.sh
@@ -51,7 +51,7 @@ do
         printf "static void usage();\n"
         printf "static int di(int i)\t\t{ exit(i);  return 1; }\n"
         printf "static int ds(string s)\t\t{ cout << s << \"\\\\n\"; return 1; }\n"
-        printf "static int dv(vector<string> v)\t\t{ for(size_t i = 0; i < v.size(); i++) { cout << v[i] << \"\\\\n\"; } return 1; }\n"
+        printf "static int dv(vector<string> v)\t\t{ for(size_t i = 0; i < v.size(); i++) { cout << v[i] << \"\\\\n\"; } exit(v.empty()); }\n"
         printf "static int dvpsl(vector<pair<string, long int> > v)\t{ for(size_t i = 0; i < v.size(); i++) { cout << v[i].first << "'"\\t"'" << v[i].second << \"\\\\n\"; } return 1; }\n"
         printf "static int dvmsvs(map<string,vector<string> > m)\t{for( map<string,vector<string> >:: iterator i  = m.begin(); i != m.end(); ++i) { cout << i->first << "'":\\n"'"; for (size_t j = 0; j < i->second.size(); ++j) { cout << "'"\\t"'" <<  i->second[j] << "'"\\n"'";}  } return 1; }\n"
         printf "static vector<string> argvec(int argc, char **argv) { vector<string> v; for(int i = 0; i < argc; i++ ) { v.push_back(argv[i]); } return v; }\n"

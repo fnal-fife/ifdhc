fb735527e3a60c4ab315ac379e4868e7b9a2165b
Marc Mengel <mengel@fnal.gov>
2015-08-27 16:59:52 -0500

    add IFDH_STASH_CACHE override in FetchShared

--- ifdh/ifdh_cp.cc
+++ ifdh/ifdh_cp.cc
@@ -2410,6 +2410,9 @@ ifdh::fetchSharedFiles( vector<pair<string,long> > list, string schema ) {
    vector<pair<string,long> >  res;
    string f;
    string rdpath("root://fndca.fnal.gov:1094/pnfs/fnal.gov");
+   if (getenv("IFDH_STASH_CACHE")) {
+       rdpath = getenv("IFDH_STASH_CACHE");
+   }
 
    if (_debug) cerr << "fetchShared -- schema is " << schema << endl;
 

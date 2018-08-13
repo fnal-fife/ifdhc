957871e506af81ff8e713998d17dbe4757e09a0e
Marc Mengel <mengel@fnal.gov>
2018-08-13 15:50:10 -0500

    sigh

--- tests/test_multi_getproxy.sh
+++ tests/test_multi_getproxy.sh
@@ -4,12 +4,15 @@ export EXPERIMENT=samdev
 GROUP=fermilab
 proxyfile=`ifdh getProxy`
 
-for try in 1 2 3 4 5 6 7 8 9 10
+for k in 0 1 2 3 4 5 6 7 8 9
 do
+for j in 0 1 2 3 4 5 6 7 8 9
+do
+    try=$k$j
     #setup
     rm -f /tmp/x509up_u${UID} $proxyfile
 
-    for i in 1 2 3 4 5
+    for i in 1 2 3 4 
     do
         ifdh getProxy > /dev/null &
     done
@@ -17,4 +20,5 @@ do
 
     printf "Try $try: "
     voms-proxy-info -all -file $proxyfile | grep $GROUP/Role=Analysis
+done
 done

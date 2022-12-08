89cc7d1dc346bbdee75de9c696420e9ca8fc185c
Marc Mengel <mengel@fnal.gov>
2022-12-08 13:34:33 -0600

    draft of script to keep authentication refreshed.

---
+++ ifdh/auth_session.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while [ $# -gt 1 ]
+do
+case x$1 in
+x--experiment) export EXPERIMENT=$2; shift; shift;;
+x*)            echo "unknown argument $1"; shift;;
+esac
+done
+export watchpid=$PPID
+
+# get initial token & proxy
+
+export IFDH_TOKEN_ENABLE=1
+export IFDH_PROXY_ENABLE=1
+
+ifdh getToken
+ifdh getProxy
+
+# check/refresh every 15 minutes in background until
+# our parent process exits
+(
+   while kill -0 $watchpid
+   do
+      # check every 15 minutes
+      sleep 900
+      ifdh getToken
+      ifdh getProxy
+   done
+) &
+
+exit 0
--- util/ifdh_version.h
+++ util/ifdh_version.h
@@ -1,1 +1,1 @@
-#define IFDH_VERSION "v2_6_10"
+#define IFDH_VERSION "v2_6_10-4-g25f25a7"
